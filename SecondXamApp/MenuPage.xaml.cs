a0a4038556a1f0242107de3edcdd7bb7d480f700
Natixis <natixis-dev@Natixiss-MacBook-Pro.local>
2017-12-05 18:29:46 +0100

    First commit

---
+++ SecondXamApp/MenuPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SecondXamApp
+{
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Device.OpenUri(new System.Uri("http://www.google.com"));
+        }
+
+        void MunabeesBtnClicked(object sender, System.EventArgs e)
+        {
+            Device.OpenUri(new System.Uri("http://www.munabees.com"));
+        }
+
+        void OserBtnClicked(object sender, System.EventArgs e)
+        {
+            Device.OpenUri(new System.Uri("http://www.oserlafrique.com"));
+        }
+
+    }
+}
---
+++ SecondXamApp/SecondXamAppPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace SecondXamApp
+{
+    public partial class SecondXamAppPage : ContentPage
+    {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //DisplayAlert("Title", "Congrats !!", "Pff! Exit");
+            Navigation.PushModalAsync(new MenuPage());
+        }
+
+        public SecondXamAppPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
